2e94b52ef9c79d7e1ec9769e15810bae03b1bf4f
UltraQbik <no1skill@yandex.ru>
2025-06-04 21:18:55 +0300

    Add printout messages

--- main.py
+++ main.py
@@ -4,8 +4,30 @@ Main file
 
 
 def main():
-    pass
+    # print welcome message
+    print(f"{'[ Welcome to the very cool irrational number memorizer ]':=^90}")
+    while True:
+        # print input message
+        print(f" {'[ 1 ] Pi constant': <24} | 3.1415...")
+        print(f" {'[ 2 ] Euler\'s constant': <24} | 2.7182...")
+        print(f" {'[ 3 ] square root of 2': <24} | 1.4142")
+        print(f" {'[ 4 ] Golden ratio': <24} | 1.6180...")
+        print(f" [ 0 ] Exit")
+        print(f"{'[ choose one of the irrational numbers ]':=^90}")
+
+        # ask for input
+        try:
+            user_input = int(input("> "))
+        except ValueError:
+            continue
+
+        # if input within range -> break
+        if -1 < user_input <= 4:
+            break
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
