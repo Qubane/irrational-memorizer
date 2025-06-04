83e8f26ef549908bfb017ecf975365187159270b
UltraQbik <no1skill@yandex.ru>
2025-06-04 21:30:05 +0300

    Refactor to use CONSTANTS list

--- main.py
+++ main.py
@@ -31,14 +31,20 @@ def main():
     # initialize with 0
     user_input = 0
 
+    # pretty print magic numbers
+    max_number_length = len(str(len(CONSTANTS)))
+    max_name_length = max(len(x["name"]) for x in CONSTANTS)
+
     # print welcome message
     print(f"{'[ Welcome to the very cool irrational number memorizer ]':=^90}")
     while True:
+        # output all constant options
+        for idx, constant in enumerate(CONSTANTS):
+            print(f" [ {idx + 1: >{max_number_length}} ] "
+                  f"{constant['name']: <{max_name_length}} "
+                  f"| {constant['preview']}...")
+
         # print input message
-        print(f" {'[ 1 ] Pi constant': <24} | 3.1415...")
-        print(f" {'[ 2 ] Euler\'s constant': <24} | 2.7182...")
-        print(f" {'[ 3 ] square root of 2': <24} | 1.4142...")
-        print(f" {'[ 4 ] Golden ratio': <24} | 1.6180...")
         print(f" [ 0 ] Exit")
         print(f"{'[ choose one of the irrational numbers ]':=^90}")
 
